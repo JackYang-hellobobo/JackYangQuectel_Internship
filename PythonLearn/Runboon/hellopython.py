8415dd7b29ba1b5be9bd2c1f62b962fcf3230eec
JackYang <915988465@qq.com>
2022-07-23 10:44:19 +0800

    Upload doc

---
+++ PythonLearn/Runboon/hellopython.py
@@ -0,0 +1,33 @@
+# print("Hello World")
+'''
+python --verrsion
+
+python enter in REPL
+
+# python xxxx.py  to introduce the .py files 
+
+'''
+
+# 多行语句可以用\切换
+# total = 1 + \
+#     2 + \
+#     3
+# print(total)
+
+# Python里面只有4种数据类型 整型 布尔型 浮点型 复数
+# int()
+# float()
+# bool()
+# complex()
+
+# Python 字符串类型String
+'' '' ''
+# '''''' 可以指定一个多行字符串
+# \ 转义
+# 反斜杠可以用来转义，可以使用r参数可以让反斜杠不发生转义。如 r"this is a line with \n" \n会显示，并不是换行。
+# 字符串可以用 ”+“ 运算符链接在一起 用”*“ 运算符重复
+#  Python 字符串有两种索引方式，从左到右以0开始，从右边往左以-1开始
+#  Python中的字符串不能改变
+# Python没有单独的字符串类型 一个字符就是长度为1的字符串
+# 字符串的截取的语法格式如下 ： 变量[头下标：尾下标：步长]  str.split ?
+
