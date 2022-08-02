33da3f33c86d1759b806091a1a3a993a84515512
JackYang <915988465@qq.com>
2022-08-02 21:36:25 +0800

    init

--- PythonLearn/Runboon/0802/Turple_Learn.py
+++ PythonLearn/Runboon/0802/Turple_Learn.py
@@ -2,4 +2,26 @@
 # for x in tup1:
 # 	print(x)
 
+# tur = (10,)
+# print(type(tur))
+# # //元组中只包含一个元素时，需要在元素后面添加逗号 , ，否则括号会被当作运算符使用：
 
+# tup1 = (12, 34, 56)
+# tup2 = ('abc', 1)
+#
+# # tup1[0] = 1
+# # Tuple 不支持修改赋值
+# tup3 = tup1+tup2
+# print(tup3)
+#
+# #  元组里面可以放多种数据类型
+
+# tup = (1,2,3,4,5,6)
+# print(tup)
+# del tup[0]
+# del  tup # 元组被彻底删除了就没有定义了咯
+# print(tup)
+
+tuple2 = (1, 4, 1)
+print(type(tuple2), type(tuple2[1]))
+print(max(tuple2))
